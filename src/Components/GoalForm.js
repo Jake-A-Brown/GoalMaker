d2e6c2a17bdbd50913f0e941da5daaeff855b25d
Jake Brown <JakeBrown@jbrownjake.com>
2023-06-09 11:15:20 -0400

    adjustments

--- src/Components/GoalForm.js
+++ src/Components/GoalForm.js
@@ -9,6 +9,11 @@ const GoalForm = ({ addGoal }) => {
     deadline: '',
     description: '',
   });
+  const [isFormVisible, setFormVisible] = useState(false);
+
+  const toggleFormVisibility = () => {
+    setFormVisible(!isFormVisible);
+  };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -22,61 +27,69 @@ const GoalForm = ({ addGoal }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="goal-form">
-      <div className="form-group">
-        <label htmlFor="name">Goal Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={goal.name}
-          onChange={handleChange}
-          placeholder="Enter goal name"
-          required
-          className="form-control"
-        />
+    <div className="goal-form-container">
+      <div className="card-header">
+        <h2>Goal Form</h2>
+        <button className="toggle-button" onClick={toggleFormVisibility}>
+          {isFormVisible ? 'Hide Form' : 'Show Form'}
+        </button>
       </div>
-      <div className="form-group">
-        <label htmlFor="category">Category</label>
-        <input
-          type="text"
-          id="category"
-          name="category"
-          value={goal.category}
-          onChange={handleChange}
-          placeholder="Enter category"
-          required
-          className="form-control"
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="deadline">Deadline</label>
-        <input
-          type="date"
-          id="deadline"
-          name="deadline"
-          value={goal.deadline}
-          onChange={handleChange}
-          required
-          className="form-control"
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="description">Description</label>
-        <textarea
-          id="description"
-          name="description"
-          value={goal.description}
-          onChange={handleChange}
-          placeholder="Enter description"
-          className="form-control"
-        ></textarea>
-      </div>
-      <button type="submit" className="btn">Add Goal</button>
-    </form>
+      {isFormVisible && (
+        <form onSubmit={handleSubmit} className="goal-form">
+          <div className="form-group">
+            <label htmlFor="name">Goal Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={goal.name}
+              onChange={handleChange}
+              placeholder="Enter goal name"
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="category">Category</label>
+            <input
+              type="text"
+              id="category"
+              name="category"
+              value={goal.category}
+              onChange={handleChange}
+              placeholder="Enter category"
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="deadline">Deadline</label>
+            <input
+              type="date"
+              id="deadline"
+              name="deadline"
+              value={goal.deadline}
+              onChange={handleChange}
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              name="description"
+              value={goal.description}
+              onChange={handleChange}
+              placeholder="Enter description"
+              className="form-control"
+            ></textarea>
+          </div>
+          <button type="submit" className="btn">Add Goal</button>
+        </form>
+      )}
+    </div>
   );
 };
 
 export default GoalForm;
-
-
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,32 +1,17 @@
-import React, { useState } from 'react';
-import GoalForm from './GoalForm';
-import GoalList from './GoalList';
+
 import ProgressBar from './ProgressBar';
 import './Home.css';
 
-const Home = ({ addGoal, goals, completeGoal, calculateProgress }) => {
-  const [isFormVisible, setFormVisible] = useState(false);
-
-  const toggleFormVisibility = () => {
-    setFormVisible(!isFormVisible);
-  };
+const Home = ({ calculateProgress }) => {
 
   return (
     <div className="card-container">
       <div className="card">
         <div className="card-header">
-          {/* <h2>Goal Form</h2>
-          <button className="toggle-button" onClick={toggleFormVisibility}>
-            {isFormVisible ? 'Hide Form' : 'Show Form'}
-          </button> */}
         </div>
-        {isFormVisible && (
           <div className="card-body">
-            {/* <GoalForm addGoal={addGoal} /> */}
           </div>
-        )}
       </div>
-      {/* <GoalList goals={goals} completeGoal={completeGoal} /> */}
       <ProgressBar progress={calculateProgress()} />
     </div>
   );
